fba1fd455336c453d7ba905000e83761e037a8fd
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-09-01 02:30:22 -0300

    in home now products are loaded by date and sales

---
+++ frontend/src/components/shop/Card.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Card = ({name, description, img, price, comparePrice}) => {
+
+    return(
+        <>
+        <div className="col-lg-4">
+            <div className="card card-product">
+                <div className="card-body">
+                <div className="text-center position-relative ">
+                    <div className=" position-absolute top-0 start-0">
+                    <span className="badge bg-danger">Sale</span>
+                    </div>
+                    <a href="#!"> <img src={img} alt="Grocery Ecommerce Template"
+                        className="mb-3 img-fluid"/></a>
+                    <div className="card-product-action">
+                    <a href="#!" className="btn-action" data-bs-toggle="modal" data-bs-target="#quickViewModal"><i
+                        className="bi bi-eye" data-bs-toggle="tooltip" data-bs-html="true" title="Quick View"></i></a>
+                    <a href="#!" className="btn-action" data-bs-toggle="tooltip" data-bs-html="true" title="Wishlist"><i
+                        className="bi bi-heart"></i></a>
+                    <a href="#!" className="btn-action" data-bs-toggle="tooltip" data-bs-html="true" title="Compare"><i
+                        className="bi bi-arrow-left-right"></i></a>
+                </div>
+                    </div>
+                <div className="text-small mb-1"><a href="#!" className="text-decoration-none text-muted"><small>{name}</small></a></div>
+                <h2 className="fs-6"><a href="../../pages/shop-single.html" className="text-inherit text-decoration-none">Haldiram's
+                    Sev Bhujia</a></h2>
+                <div>
+                <small className="text-warning"> <i className="bi bi-star-fill"></i>
+                <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-fill"></i>
+                    <i className="bi bi-star-half"></i></small> <span className="text-muted small">4.5(149)</span>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-3">
+                    <div><span className="text-dark">${price}</span> <span
+                    className="text-decoration-line-through text-muted">${comparePrice}</span>
+                    </div>
+                    <div><a href="#!" className="btn btn-primary btn-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    className="feather feather-plus">
+                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg> Add</a></div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Card;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import Layout from '../hocs/Layout';
-const Home = () => {
+import Layout from "../hocs/Layout"
+import { connect } from 'react-redux';
+import { 
+    get_products_by_arrival, 
+    get_products_by_sold 
+} from '../redux/actions/products';
+import Card from '../components/shop/Card';
+import {Container, Row, Col} from 'react-bootstrap';
+import { useEffect } from "react";
+
+const Home = ({
+    get_products_by_arrival, 
+    get_products_by_sold,
+    products_arrival,
+    products_sold
+}) => {
+
+    console.log(products_arrival)
+
+    console.log(products_sold)
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+
+        get_products_by_arrival();
+        get_products_by_sold();
+    }, []);
+    
 
     return(
         <Layout>
-            home
+            <Container>
+                <Row>
+                    <Col xs={6}>
+                        Lo mas Reciente
+                        {products_arrival && products_arrival.map((product, i) => (
+                            <Card key={i} name={product.name} description={product.description} 
+                            img={product.get_thumbnail} price={product.price} comparePrice={product.compare_price}/>
+                        ))
+
+                        }
+                    </Col>
+                    <Col xs={6}>
+                        Lo mas vendido
+                        {products_sold && products_sold.map((product, i) => (
+                            <Card key={i} name={product.name} description={product.description} 
+                            img={product.get_thumbnail} price={product.price} comparePrice={product.compare_price}/>
+                        ))
+
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </Layout>
     )
 }
 
+const mapStateToProps = state =>  ({
+    products_arrival: state.Products.products_arrival,
+    products_sold: state.Products.products_sold,
+})
 
-export default Home;
+export default connect(mapStateToProps,{
+    get_products_by_arrival, 
+    get_products_by_sold,
+}) (Home);
