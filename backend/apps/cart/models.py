09f1a09fb28267e5a892483eee1070fb80dec9d8
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-09-05 19:38:06 -0300

    views and models of the cart made

--- backend/apps/cart/models.py
+++ backend/apps/cart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from apps.product.models import Product
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_items = models.IntegerField(default=0)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField()
