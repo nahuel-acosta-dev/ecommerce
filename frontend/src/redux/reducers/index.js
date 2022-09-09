a72487e58ce6e0c8d3da653adef7e7133016f418
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-09-08 22:17:25 -0300

    shipping added to redux

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -4,11 +4,13 @@ import Alert from './alert';
 import Categories from './categories';
 import Products from './products';
 import Cart from './cart';
+import Shipping from './shipping';
 
 export default combineReducers({
     Auth,
     Alert,
     Categories, 
     Products,
-    Cart
+    Cart,
+    Shipping
 })
